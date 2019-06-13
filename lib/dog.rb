b80467b39c90365d7484322aad1382dc47f14d44
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-13 22:34:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
-  attr_accessor :name
+  # Could also use attr_accessor like below:
+  # attr_accessor :name
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
 
   def bark
     puts "woof!"
   end
-
-
 end
